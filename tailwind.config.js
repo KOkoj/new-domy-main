8efa60aa8c5c8c43d46fcd8964514063f6dc43e4
lucacr90-boop <lucacr90@gmail.com>
2026-02-09 13:37:12 +0100

    Tailwind: exclude ./app/page.js from content scan to avoid Windows EBUSY on renamed file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-/** @type {import('tailwindcss').Config} */
+﻿/** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: ["class"],
-    content: [
+    content: [\n      '!./app/page.js',
       './pages/**/*.{js,jsx}',
       './components/**/*.{js,jsx}',
       './app/**/*.{js,jsx}',
